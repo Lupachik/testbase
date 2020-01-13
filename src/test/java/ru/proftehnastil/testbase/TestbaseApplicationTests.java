77c6fdabac09093df76b3104d6fe1113ebfd48a2
Lupachik <lupachik@gmail.com>
2020-01-14 01:15:57 +0300

    initial project

---
+++ src/test/java/ru/proftehnastil/testbase/TestbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.proftehnastil.testbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
